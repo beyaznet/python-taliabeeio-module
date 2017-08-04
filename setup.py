3d6e812d46e59a83c997cbff433fe192d589c8ad
emrah <emrah.com@gmail.com>
2017-08-04 12:20:42 +0300

    update distribution metadata

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
-setup(name='taliabeeio',
-      version='0.1',
-      packages=['taliabeeio'],
-      description='Python TaliaBeeIO module',
-      author = 'Beyaz R&D Team',
-      author_email = 'arge@beyaz.net',
-      url = 'https://github.com/beyaznet/python-taliabeeio-module',
-      keywords = ['raspberry', 'raspberry pi', 'medioex', 'taliabee'],
-      install_requires=['requests'])
+setup(
+    name='taliabeeio',
+    version='0.1.1',
+    packages=['taliabeeio'],
+    description='Python TaliaBeeIO module',
+    url = 'https://github.com/beyaznet/python-taliabeeio-module',
+    author = 'Beyaz R&D Team',
+    author_email = 'arge@beyaz.net',
+    license='MIT',
+    keywords = 'raspberry pi medioex taliabee',
+    install_requires=['requests'],
+    python_requires='>=3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+)
