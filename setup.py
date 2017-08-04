cd683b3cb3bb43a7dbca6df1966c25174d93210e
emrah <emrah.com@gmail.com>
2017-08-04 10:34:51 +0300

    module added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='taliabeeio',
+      version='0.1',
+      packages=['taliabeeio'],
+      description='Python TaliaBeeIO module',
+      author = 'Beyaz R&D Team',
+      author_email = 'arge@beyaz.net',
+      url = 'https://github.com/beyaznet/python-taliabeeio-module',
+      keywords = ['raspberry', 'raspberry pi', 'medioex', 'taliabee'],
+      install_requires=['requests'])
